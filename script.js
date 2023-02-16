04273b9c7960cd4a0efe9a7d5739158f283d2805
Naser <2022naserahmadi@gmail.com>
2023-02-16 19:35:03 +0330

    lasted

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ console.log(fName,lName);
 const news="naser";
 console.log("news");
 console.log(news);
+/*null*/
+let value=null;
+console.log(null); 
+/*undefiend*/
+value=undefined;
+console.log(value);
+value;
+console.log(value);
+/*String*/
+value="motherFucker";
+console.log(value);
+/*integer*/
+value=11;
+console.log(value);
+const lol=11+12;
+//lol=34;
+console.log(lol);
