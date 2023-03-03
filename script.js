619525444a09d69931a171a15193648d27babcfc
Hill <125409221+NARI1108@users.noreply.github.com>
2023-03-03 17:23:37 +0330

    Math's Method

--- script.js
+++ script.js
@@ -4,9 +4,22 @@ const circle={
         console.log("draw");
     }
 }
-
-if('radius' in circle){
-    console.log("yea");
-    console.log('radius' in circle);
+const another ={};
+for(let key in circle){
+   another[key]=another[key];
 }
-
+console.log(another);
+const another1 = Object.assign({Color:"red",circle});
+console.log(another1);
+const another2 ={location:2,...circle};
+console.log(another2);
+console.log(Math.PI);
+console.log(Math.random());
+function getRandomArbitrary(max,min){
+    return Math.floor( Math.random()*(max-min)+min);
+}
+console.log(getRandomArbitrary(10,4));
+console.log(Math.round(1.2));
+console.log(Math.max(1,2,3,4,5,6,7,8,9,0));
+console.log(Math.min(1,2,3,4,5,6,67,7,8));
+console.log(Math.abs(-22));
