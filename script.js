0ff446130e1f2027439a29484ba1b0c813982a59
Hill <125409221+NARI1108@users.noreply.github.com>
2023-03-07 19:00:42 +0330

    ResearchingTheElementAtArrays

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-const number =[2,3,4];
-console.log(number);
-number.push(1);
-console.log(number);
-number.push=333;
-console.log(number);
-number.unshift(1111);
-console.log(number);
-number.unshift=22222;
-console.log(number);
-number.splice(1,0,"Naser");
-console.log(number);
-number.splice(1,1,"reyhaneh");
-console.log(number);
+const number=[1,2,3,2,4];
+console.log(number.indexOf(1));
+console.log(number.indexOf(3));
+console.log(number.indexOf(5));
+console.log(number.indexOf("2"))
+console.log(number.indexOf(2));
+console.log(number.lastIndexOf(2));
+const order =[5,2,3,45,5];
+if(order.lastIndexOf(45) !== 3){
+    console.log("yes");
+}else{
+console.log("No");}
+console.log(order.includes(11));
+console.log(order.indexOf(5,1));
+const Courses=[
+    {id:1 , name:"a"},
+    {id:2 , name:"b"}
+]
+const result = Courses.find(function(course){
+    return course.name === "v";
+});
+console.log(result);
+const deads=[
+    {id:1 , name:"c"},
+    {id:2 , name:"g"}
+]
+const die = deads.findIndex(function(dead)
+{
+    return dead.name ==="g";
+}
+)
+console.log(die);
