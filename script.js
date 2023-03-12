d41f8e563ce8e694b0d1647f78e419a9e72621c3
Hill <125409221+NARI1108@users.noreply.github.com>
2023-03-12 23:57:40 +0330

    Combined The Double Arrays

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-let number =[1,2,3,4,5];
-let another = number;
-number.splice(0,number.length);
-console.log(number);
-console.log(another);
-//////////////////////////////
-let array = [1,2,3,4];
-array.length=0;
-console.log(array);
+const array1= [1,2,3,4];
+const array2=[5,6,7,8];
+const array3 = array1.concat(array2);
+console.log(array3);
+const array4 = array3.slice(2,4);
+console.log(array4);
