8d44692f90b7a42ac3a56460c898915939662b73
Hill <125409221+NARI1108@users.noreply.github.com>
2023-03-13 00:08:17 +0330

    Using The Concat Method

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const array1= [1,2,3,4];
-const array2=[5,6,7,8];
-const array3 = array1.concat(array2);
-console.log(array3);
-const array4 = array3.slice(2,4);
-console.log(array4);
+
+const first = [{id:1}];
+const second = [5,6,7,8];
+const combined = first.concat(second);
+first[0].id=10
+console.log(combined);
+console.log(first);
